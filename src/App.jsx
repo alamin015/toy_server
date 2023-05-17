00d55f25474cfabd619a28da2a204867e438e96b
Al Amin <alamin>
2023-05-17 23:49:51 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+    <h3 className='text-4xl'>hello world</h3>
+    <p>hello</p>
+    </>
+  )
+}
+
+export default App
